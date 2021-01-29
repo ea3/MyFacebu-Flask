dd1a7d1a80d3046633beb3b39479218c58fe6c3b
Emilio Araos <ea3dev@gmail.com>
2021-01-29 02:48:56 -0800

    about and home pages

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+posts = [
+    {
+        'author': 'Emilio',
+        'title': 'Blog 1',
+        'content': 'This is a post',
+        'date_posted': 'January 2021'},
+    {
+        'author': 'Mariela',
+        'title': 'Blog 2',
+        'content': 'This is another post',
+        'date_posted': 'February 2021'
+    }
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
